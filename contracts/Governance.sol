779afe650ed132bfbe8534a66404a511dad9b57e
tallywiesenberg <t.j.wies@gmail.com>
2022-06-22 14:09:43 -0400

    remove unused getters

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.3;
 
-import "./interfaces/ITellorFlex.sol";
+import "./interfaces/IOracle.sol";
 import "./interfaces/IERC20.sol";
 import "usingtellor/contracts/UsingTellor.sol";
 import "hardhat/console.sol";
